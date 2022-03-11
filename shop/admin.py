a1c31401a1d36d8d9e05474a54899d57f88bad31
VU QUANG TON <vuquangton@outlook.com>
2022-03-11 11:11:12 +0700

    commit lan 2

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import Category, Product
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {
+        'slug':('name',)
+    }
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','slug', 'price',
+                    'availble','created','update']
+    list_filter =['availble','created','update']
+    list_editable = ['price','availble']
+    prepopulated_fields = {
+        'slug':('name',)
+    }
+        
+    
+        
