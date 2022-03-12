b7775a9b3a941c06af84aee62160aeeec50bfa7d
vqton <vuquangton@outlook.com>
2022-03-12 20:31:14 +0700

    creating shopping cart

--- shop/admin.py
+++ shop/admin.py
@@ -12,9 +12,9 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name','slug', 'price',
-                    'availble','created','update']
-    list_filter =['availble','created','update']
-    list_editable = ['price','availble']
+                    'available','created','updated']
+    list_filter =['available','created','updated']
+    list_editable = ['price','available']
     prepopulated_fields = {
         'slug':('name',)
     }
--- shop/models.py
+++ shop/models.py
@@ -36,9 +36,9 @@ class Product(models.Model):
                               blank = True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    availble = models.BooleanField(default=True)
+    available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now = True)
+    updated = models.DateTimeField(auto_now = True)
     
     class Meta:
         ordering = ('name',)
